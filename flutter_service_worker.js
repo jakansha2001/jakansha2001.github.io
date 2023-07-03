73f993d768da935172fe0837e6fcb7913350b034
jakansha2001 <jakansha2001@gmail.com>
2023-07-04 01:09:38 +0530

    build: July 4th 23

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "4aa78f0c7b0a33a604db17d5f6893baa",
-"index.html": "3e3bbdd88e103a47d850cd9a52e5bde0",
-"/": "3e3bbdd88e103a47d850cd9a52e5bde0",
-"main.dart.js": "6c184239b074dbe7c4b8c8f384befba0",
+"index.html": "22d368e277e198bd1524732e7f7967ec",
+"/": "22d368e277e198bd1524732e7f7967ec",
+"main.dart.js": "acd38761e60a8543ad834a2d12411871",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "cec06261144dd5f33864440510a04c82",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
